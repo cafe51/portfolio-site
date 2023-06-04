ebc530ef2a4f19b1d9f042f38cf5273dab480208
cafe51 <cafecafe51@hotmail.com>
2023-06-03 21:32:13 -0400

    navbar criada

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,28 @@
-const Header = () => {
+'use client';
+import { useState } from 'react';
+import Navbar from './Navbar';
 
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <header className='bg-gray-800 fixed w-full transition-all duration-500 z-50 px-8 md:px-16 py-3 md:pr-64 lg:pr-16'>
-            <div className="flex justify-between items-center mx-auto">
+        <header className='bg-gray-800 fixed w-full transition-all duration-500 z-50 px-8 md:px-16 py-3 md:py-0 md:pr-64 lg:pr-16'>
+            <div className="flex justify-between items-center ">
                 <div>
                     <p className="text-2xl font-bold text-white">Japhé</p>
                 </div>
+                <button
+                    className="md:hidden block text-white"
+                    onClick={ () => setIsMenuOpen(!isMenuOpen) }
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                        { isMenuOpen ? (
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={ 2 } d="M6 18L18 6M6 6l12 12" />
+                        ) : (
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={ 2 } d="M4 6h16M4 12h16M4 18h16" />
+                        ) }
+                    </svg>
+                </button>
+                <Navbar isMenuOpen={ isMenuOpen } setIsMenuOpen={ setIsMenuOpen } />
             </div>
         </header>
     );
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+interface NavbarProps {
+  isMenuOpen: boolean;
+  setIsMenuOpen: (isOpen: boolean) => void;
+}
+
+const navItemsStyle = 'text-white hover:text-gray-800 hover:bg-gray-200 transition-colors duration-300 p-2';
+
+const Navbar: React.FC<NavbarProps> = ({ isMenuOpen, setIsMenuOpen }) => {
+    return (
+        <nav className={ `transition-all duration-500 fixed w-2/5 md:w-2/5 sm:w-1/3 h-screen z-40 top-14 right-0 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0 md:static md:h-auto py-4` }>
+            <div className="w-full h-full">
+                <div className="flex flex-col space-y-8 py-8 px-6 md:flex-row md:space-y-0 md:space-x-4 md:py-0 md:px-0">
+                    <a href="#about" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Início</a>
+                    <a href="#skills" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Habilidades</a>
+                    <a href="#projects" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Projetos</a>
+                    <a href="#certificates" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Certificações</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
