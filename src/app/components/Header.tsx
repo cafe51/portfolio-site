c05ffa9de6ad821df92eed8e703f6675d59d2db7
cafe51 <cafecafe51@hotmail.com>
2023-06-03 21:05:07 -0400

    header criado

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+
+    return (
+        <header className='bg-gray-800 fixed w-full transition-all duration-500 z-50 px-8 md:px-16 py-3 md:pr-64 lg:pr-16'>
+            <div className="flex justify-between items-center mx-auto">
+                <div>
+                    <p className="text-2xl font-bold text-white">Japhé</p>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
