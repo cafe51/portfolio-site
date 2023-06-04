973170ac68b65e63941bc3bcd552f50e87f245e7
cafe51 <cafecafe51@hotmail.com>
2023-06-03 21:36:37 -0400

    espaçamento dos itens da navbar ajustado

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,8 +8,8 @@ const navItemsStyle = 'text-white hover:text-gray-800 hover:bg-gray-200 transiti
 const Navbar: React.FC<NavbarProps> = ({ isMenuOpen, setIsMenuOpen }) => {
     return (
         <nav className={ `transition-all duration-500 fixed w-2/5 md:w-2/5 sm:w-1/3 h-screen z-40 top-14 right-0 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0 md:static md:h-auto py-4` }>
-            <div className="w-full h-full">
-                <div className="flex flex-col space-y-8 py-8 px-6 md:flex-row md:space-y-0 md:space-x-4 md:py-0 md:px-0">
+            <div>
+                <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-2">
                     <a href="#about" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Início</a>
                     <a href="#skills" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Habilidades</a>
                     <a href="#projects" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Projetos</a>
