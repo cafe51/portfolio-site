1acf43d1633d08a8fe0fadecb8b0db1a7e475733
cafe51 <cafecafe51@hotmail.com>
2023-06-03 22:23:22 -0400

    adicionado componente de titulo

---
+++ src/app/subComponents/ProfileTitleCard.tsx
@@ -0,0 +1,10 @@
+const ProfileTitleCard = () => {
+    return (
+        <div>
+            <h1>Japhé Nogueira</h1>
+            <h2>Desenvolvedor Web</h2>
+        </div>
+    );
+};
+
+export default ProfileTitleCard;
