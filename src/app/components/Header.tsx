19d7b7016efe9d44b0501c928d90fa2fa5ce84fc
cafe51 <cafecafe51@hotmail.com>
2023-06-04 14:23:37 -0400

    adicionado transparência para o header ao scrollar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,24 @@
 'use client';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+  
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        offset > 200 ? setIsScrolled(true) : setIsScrolled(false);
+    };
+  
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
+    const opacity = isScrolled ? 'bg-opacity-80' : '';
+    
     return (
-        <header className='bg-gray-800 fixed w-full transition-all duration-500 z-50 px-8 md:px-16 py-3 md:py-0 md:pr-64 lg:pr-16'>
+        <header className={ `bg-gray-800 fixed w-full transition-all duration-500 z-50 px-8 md:px-16 py-3 md:py-0 md:pr-64 lg:pr-16 ${opacity}` }>
             <div className="flex justify-between items-center ">
                 <div>
                     <p className="text-2xl font-bold text-white">Japhé</p>
