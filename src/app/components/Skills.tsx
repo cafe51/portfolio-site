b6efc5bc5b1bbb678463c9e3afac172bb0f202c3
cafe51 <cafecafe51@hotmail.com>
2023-06-04 16:21:05 -0400

    criado botão com função de expandir e retrair a lista de skills

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,21 +1,45 @@
+'use client';
 import { NextPage } from 'next';
+import { useState } from 'react';
 import skills from '../data/skills';
 import SkillCard from '../subComponents/SkillCard';
+import ExpandButton from '../subComponents/ExpandButton';
 
 const Skills: NextPage = () => {
+    const [itemsToShow, setItemsToShow] = useState(4);
+    const [expanded, setExpanded] = useState(false);
+
+    const showMore = () => {
+        setItemsToShow(skills.length);
+        setExpanded(true);
+    };
+
+    const showLess = () => {
+        setItemsToShow(4);
+        setExpanded(false);
+    };
+
     return (
-        <section id='skills'>
-            <div>
-                <h2>Habilidades</h2>
-                { skills.map((skill) => (
-                    <SkillCard skillData={ skill } key={ skill.id } />
-                )) }
+        <section id='skills' className="bg-gray-200 p-8 relative shadow-xl rounded-md">
+            <div className="bg-white mx-auto p-8 rounded-xl">
+                <h2 className="mb-6">Habilidades</h2>
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
+                    { skills.slice(0, itemsToShow).map((skill) => (
+                        <SkillCard skillData={ skill } key={ skill.id } />
+                    )) }
+                </div>
+                { skills.length > 4 && (
+                    <ExpandButton
+                        expanded={ expanded }
+                        onExpand={ showMore }
+                        onCollapse={ showLess }
+                    />
+                ) }
             </div>
         </section>
     );
 };
 
 export default Skills;
-
 
 
---
+++ src/app/subComponents/ExpandButton.tsx
@@ -0,0 +1,20 @@
+interface ExpandButtonProps {
+  expanded: boolean;
+  onExpand: () => void;
+  onCollapse: () => void;
+}
+
+const ExpandButton: React.FC<ExpandButtonProps> = ({ expanded, onExpand, onCollapse }) => {
+    return (
+        <div className="flex justify-center mt-6">
+            <button
+                onClick={ expanded ? onCollapse : onExpand }
+                className="transition-all duration-500 px-4 py-2 bg-gray-800 text-white rounded-md text-lg hover:bg-gray-700 w-full sm:w-auto"
+            >
+                { expanded ? 'Ver menos' : 'Ver mais' }
+            </button>
+        </div>
+    );
+};
+
+export default ExpandButton;
