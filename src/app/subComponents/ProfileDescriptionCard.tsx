18d81aba2d349ce5ae75007200e28636ba92822c
cafe51 <cafecafe51@hotmail.com>
2023-06-03 22:21:51 -0400

    adicionado componente de descrição

---
+++ src/app/subComponents/ProfileDescriptionCard.tsx
@@ -0,0 +1,25 @@
+const ProfileDescriptionCard = () => {
+    return (
+        <div>
+            <div>
+                <p>
+                  Olá, meu nome é Japhé Nogueira e sou desenvolvedor web com experiência em
+                  desenvolvimento frontend e backend. Eu me especializei em React, Node.js,
+                  Next.js e outros frameworks e bibliotecas modernas para criar aplicações
+                  web de alto desempenho.
+                </p>
+                <p>
+                  Estou sempre buscando aprender e crescer profissionalmente. Aqui, você
+                  encontrará meu portfólio de projetos e certificações que obtive ao longo da
+                  minha carreira.
+                </p>
+                <p>
+                  Quando não estou trabalhando, gosto de passar meu tempo livre com minha
+                  família e amigos, explorar novos lugares e aprender novas habilidades.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileDescriptionCard;
