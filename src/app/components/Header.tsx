4387b9620fd1e321f3791860cef888312a971a9a
cafe51 <cafecafe51@hotmail.com>
2023-06-13 23:58:22 -0400

    agora é possivel fechar o menu lateral tocando fora dele

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -32,18 +32,23 @@ const Header = () => {
                         JAPHÉ
                     </a>;
                 </div>
-                <button
-                    className="md:hidden block text-white"
-                    onClick={ () => setIsMenuOpen(!isMenuOpen) }
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-                        { isMenuOpen ? (
+                {
+                    !isMenuOpen
+                        ?
+                        <button
+                            className="md:hidden block text-white"
+                            onClick={ () => setIsMenuOpen(!isMenuOpen) }
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={ 2 } d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button>
+
+                        :
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={ 2 } d="M6 18L18 6M6 6l12 12" />
-                        ) : (
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={ 2 } d="M4 6h16M4 12h16M4 18h16" />
-                        ) }
-                    </svg>
-                </button>
+                        </svg>
+                }
                 <Navbar isMenuOpen={ isMenuOpen } setIsMenuOpen={ setIsMenuOpen } />
             </div>
         </header>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 interface NavbarProps {
   isMenuOpen: boolean;
   setIsMenuOpen: (isOpen: boolean) => void;
@@ -6,8 +8,46 @@ interface NavbarProps {
 const navItemsStyle = 'text-white hover:text-gray-800 hover:bg-gray-200 transition-colors duration-300 p-2';
 
 const Navbar: React.FC<NavbarProps> = ({ isMenuOpen, setIsMenuOpen }) => {
+    const node = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: any) => {
+            if (node.current?.contains(e.target)) {
+                return;
+            }
+            setIsMenuOpen(false);
+        };
+
+        if (isMenuOpen) {
+            document.addEventListener('mousedown', handleClickOutside);
+            document.addEventListener('touchmove', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener('touchmove', handleClickOutside);
+        };
+    }, [isMenuOpen, setIsMenuOpen]);
+
     return (
-        <nav className={ `transition-all duration-500 fixed w-2/5 md:w-2/5 sm:w-1/3 h-screen z-40 top-14 right-0 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0 md:static md:h-auto py-4` }>
+        <nav ref={ node } className={
+            `
+            sm:w-1/3
+            transition-all
+            duration-500
+            fixed w-2/5
+            h-screen
+            z-40
+            top-14
+            right-0
+            transform
+            ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}
+            md:w-2/5
+            md:translate-x-0
+            md:static
+            md:h-auto
+            py-4`
+        }>
             <div className="nav-gradient">
                 <div className="flex flex-col space-y-8 py-4 px-6 md:flex-row md:space-y-0 md:space-x-4 md:py-0 md:px-0">
                     <a href="#about" onClick={ () => setIsMenuOpen(false) } className={ navItemsStyle }>Início</a>
