3ea9ea67b9f6a4055249af6957673a8631d48765
cafe51 <cafecafe51@hotmail.com>
2023-06-16 03:09:52 -0400

    comentarios apagados de About

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,12 +1,8 @@
-/* eslint-disable react/jsx-curly-spacing */
 import { NextPage } from 'next';
-// import ProfileImageCard from '../subComponents/ProfileImageCard';
-// import ProfileDescriptionCard from '../subComponents/ProfileDescriptionCard';
 import ScrollDownButton from '../subComponents/ScrollDownButton';
 import { HiDocumentArrowDown } from 'react-icons/hi2';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-// import ProfileTitleCard from '../subComponents/ProfileTitleCard';
 
 const About: NextPage = () => {
     return (
@@ -17,11 +13,11 @@ const About: NextPage = () => {
                     <div className='flex flex-col items-center pt-3'>
                         <div className='w-full flex flex-col justify-center text-center px-2'>
                             <h1 className=' text-[28px] font-bold'>Desenvolvedor Web Full Stack</h1>
-                            {/* <h1 className=' text-[28px] font-bold'>Full Stack</h1> */}
+                            
                         </div>
                     </div>
                     <div
-                        className={`
+                        className={ `
                             bg-[url('/images/profile_mobile7.png')]
                             bg-cover
                             rounded-full
@@ -30,7 +26,7 @@ const About: NextPage = () => {
                             mt-0
                             w-[250px]
                             h-[450px]
-                            `}>
+                            ` }>
                     </div>
                     <div className='-mt-12 w-2/4 mb-12 flex flex-col justify-center items-center text-center text-base '>
                         <div className='flex flex-row gap-4'>
@@ -51,7 +47,7 @@ const About: NextPage = () => {
                     </div>
                     <div className="w-full flex flex-row items-start justify-center gap-4 pt-20">
                         <div
-                            className={`
+                            className={ `
                             bg-[url('/images/ProfileImageSite2.png')]
                             bg-cover
                             rounded-full
@@ -59,7 +55,7 @@ const About: NextPage = () => {
                             w-[300px]
                             h-[450px]
                     
-                            `}>
+                            ` }>
                         </div>
                         <div className='pt-24 flex flex-col justify-center items-start w-2/6 text-xl  '>
                             <p>Olá, meu nome é Japhé Nogueira. Sou um Desenvolvedor web Full Stack e amo muito o que faço.</p>
@@ -81,7 +77,7 @@ const About: NextPage = () => {
                     </div>
                     <div className="w-fit flex items-start justify-center pt-28 ">
                         <div
-                            className={`
+                            className={ `
                             bg-[url('/images/ProfileImageSite2.png')]
                             bg-cover
                             rounded-full
@@ -89,7 +85,7 @@ const About: NextPage = () => {
                             w-[300px]
                             h-[450px]
                            
-                            `}>
+                            ` }>
                         </div>
                         <div className='pt-36 pl-16 flex flex-col justify-center items-start w-2/6 text-2xl  '>
                             <p>Olá, meu nome é Japhé Nogueira. Sou um Desenvolvedor web Full Stack e amo muito o que faço.</p>
@@ -101,32 +97,6 @@ const About: NextPage = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className="hidden lg:flex flex-row w-4/5 justify-center ">
-                    <ProfileImageCard
-                        imageSize = { 250 }
-                        isMobile = { false }
-                        tailwindClassInDiv={ 'flex items-center' }
-                        tailwindClassInImgTag = { 'max-h-96' }
-                    />
-                    <div className="md:w-2/3 p-4 flex flex-col items-center justify-center">
-                        <div className='pt-4 w-5/6 flex flex-col items-center'>
-                            <div className='w-full flex flex-row justify-center text-center'>
-                                <h1 className=' text-[48px] font-bold'>Desenvolvedor Web Full Stack</h1>
-                            </div>
-                        </div>
-                        <div className='text-center w-full text-2xl '>
-                            <p>Olá, meu nome é Japhé Nogueira.</p>
-                            <p>Sou um Desenvolvedor web Full Stack</p>
-                            <p>e amo muito o que faço.</p>
-                            <div className='flex flex-row gap-4 justify-center p-8'>
-                                <FaGithub size={ 36 }/>
-                                <BsLinkedin size={ 36 }/>
-                                <HiDocumentDownload size={ 36 }/>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-
             </div>
             <ScrollDownButton href="#skills" />
         </section>
