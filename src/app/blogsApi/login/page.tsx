35c3898e976d97302dec9c891d90de1e04dd5432
cafe51 <cafecafe51@hotmail.com>
2023-07-11 09:32:01 -0400

    feat: tela de login

---
+++ src/app/blogsApi/login/page.tsx
@@ -0,0 +1,26 @@
+export default function Login() {
+    return(
+        <div className='w-full h-screen flex flex-col justify-center items-center'>
+            <section className='bg-gray-200 rounded shadow md:w-2/5'>
+                <form className='flex flex-col gap-10 p-6 justify-center items-center'>
+                    <div>
+                        <label className='flex gap-4'>
+                            <h2>e-mail</h2>
+                            <input
+                                type='text'
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label className='flex gap-4'>
+                            <h2>senha</h2>
+                            <input
+                                type='text'
+                            />
+                        </label>
+                    </div>
+                </form>
+            </section>
+        </div>
+    );
+}
