9ac474f742d0bdb9ad89474192212d8355b7e881
cafe51 <cafecafe51@hotmail.com>
2023-07-11 05:23:53 -0400

    feat: tipagens

---
+++ src/app/blogsApi/interfaces.tsx
@@ -0,0 +1,21 @@
+export type UserType = {
+  id?: number
+  display_name: string
+  email: string
+  image?: string
+};
+
+export type CategoryType = {
+  id?: number
+  name: string
+};
+
+export type PostType = {
+  id: number
+  title: string
+  content: string
+  user_id: number
+  published: Date
+  updated: Date
+  users: UserType
+};
