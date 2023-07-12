fe4a6f500bb2ce8f42aaf64d85688559007aedf5
cafe51 <cafecafe51@hotmail.com>
2023-07-12 17:10:53 -0400

    adiocionado fetches para as apis

---
+++ src/app/blogsApi/api.tsx
@@ -0,0 +1,125 @@
+import axios from 'axios';
+import { UserType, PostType, CategoryType } from './interfaces';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3001',
+});
+
+
+export const loginRequestApi = async(body: UserType) => {
+    const { data } = await axiosInstance.post('/login', body);
+    return data;
+};
+
+export const registerRequestApi = async(body: { email: string, password: string }) => {
+    const { data } = await axiosInstance.post('/user', body);
+    return data;
+};
+
+export const getUserByIdApi = async(token: string, id: number) => {
+    try {
+        const { data } = await axiosInstance.get(`/user${id}`, {
+            headers: { Authorization: token },
+        });
+        return data;
+    } catch(error: any) {
+        return error.response;
+    }
+    
+};
+
+export const getUsersApi = async(token: string) => {
+    try {
+        const { data } = await axiosInstance.get('/user', {
+            headers: { Authorization: token },
+        });
+        return data;
+    } catch(error: any) {
+        return error.response;
+    }
+};
+
+export const getCategoriesApi = async(token: string) => {
+    try {
+        const { data } = await axiosInstance.get('/categories', {
+            headers: { Authorization: token },
+        });
+        return data;
+    } catch(error: any) {
+        return error.response;
+    }
+};
+
+export const addNewCategoryApi = async(body: CategoryType) => {
+    const { data } = await axiosInstance.post('/post', body);
+    return data;
+};
+
+export const addNewPostApi = async(body: PostType) => {
+    const { data } = await axiosInstance.post('/post', body);
+    return data;
+};
+
+export const getPostsApi = async(token: string) => {
+    try {
+        const { data } = await axiosInstance.get('/post', {
+            headers: { Authorization: token },
+        });
+        return data;
+    } catch(error: any) {
+        return error.response;
+    }
+};
+
+export const getPostByIdApi = async(token: string, id: number) => {
+    try {
+        const { data } = await axiosInstance.get(`/post/${id}`, {
+            headers: { Authorization: token },
+        });
+        return data;
+    } catch(error: any) {
+        return error.response;
+    }
+};
+
+export const getPostByQueryApi = async(token: string, query: number) => {
+    try {
+        const { data } = await axiosInstance.get(`/post/search/?q=${query}`, {
+            headers: { Authorization: token },
+        });
+        return data;
+    } catch(error: any) {
+        return error.response;
+    }
+};
+
+export const updatePostApi = async(body: PostType, id: number) => {
+    const { data } = await axiosInstance.put(`/post/${id}`, body);
+    return data;
+};
+
+export const deleteVehicle = async(id: number) => {
+    await axiosInstance.delete(`/post/${id}`);
+};
+
+
+  
+
+// export const getVehicle = async(vehicle: string) => {
+//     const { data } = await axiosInstance.get(`${vehicle}/`);
+//     return data;
+// };
+
+// export const registerVehicle = async(vehicle: string, registerValues: CarType | MotorcycleType) => {
+//     const request = await axiosInstance.post(`${vehicle}/`, registerValues);
+//     return request;
+// };
+
+// export const deleteVehicle = async(vehicle: string, id: string) => {
+//     await axiosInstance.delete(`${vehicle}/${id}`);
+// };
+
+// export const updateVehicle = async(vehicle: string, newValues: CarType | MotorcycleType, id: string) => {
+//     const { data } = await axiosInstance.put(`${vehicle}/${id}`, newValues);
+//     return data;
+// };
--- src/app/blogsApi/login/page.tsx
+++ src/app/blogsApi/login/page.tsx
@@ -1,28 +1,110 @@
+'use client';
+
+import { useState } from 'react';
+// import { getUsersApi, loginRequestApi } from '../api';
+
 export default function Login() {
+    const [loginErrorMessage, setLoginErrorMessage] = useState(false);
+    const [registerValues, setRegisterValues] = useState({
+        email: '',
+        password: '',
+    });
+    
+    const isDisable = () => {
+        const email = registerValues.email.length > 0;
+        const password = registerValues.password.length > 0;
+        const properties = [email, password];
+        return !properties.every(property => property);
+    };
+    
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setRegisterValues({ ...registerValues, [name]: value });
+    };
+    
+    const handleSubmit = async(e: any) => {
+        try {
+            e.preventDefault();
+            // const { response } = await loginRequestApi(registerValues);
+            // const userResponse = await getUsersApi();
+            // console.log('LOGIN BEM SUCEDIDO', response);
+            // console.log('USERS', userResponse);
+        } catch({ response }: any) {
+            console.log(response.request.status, response.request.statusText, response.data.message || response.data.error);
+
+            setLoginErrorMessage(true);
+        }
+
+    };
     return(
-        <div className='w-full h-screen flex flex-col justify-center items-center'>
-            <section className='bg-gray-200 rounded shadow md:w-2/5'>
-                <form className='flex flex-col gap-10 p-6 justify-center items-center'>
-                    <div>
-                        <label className='flex gap-4'>
-                            <h2>e-mail</h2>
-                            <input
-                                className="text-center"
-                                type='text'
-                            />
-                        </label>
-                    </div>
-                    <div>
-                        <label className='flex gap-4'>
-                            <h2>senha</h2>
-                            <input
-                                className="text-center"
-                                type='text'
-                            />
-                        </label>
-                    </div>
+        <section className='w-full h-screen flex flex-col justify-center items-center'>
+            <div className='bg-gray-200 rounded shadow md:w-2/5'>
+                <form 
+                    className='flex flex-col gap-10 p-6 justify-center items-center'
+                    method="post"
+                    onSubmit={ (e) => handleSubmit(e) }
+                >
+
+                    <label className='flex gap-4' htmlFor="email">
+                        <h2>e-mail</h2>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="insira seu email"
+                            minLength={ 6 }
+                            maxLength={ 28 }
+                            onInvalid={ event => {
+                                const target = event.target as HTMLInputElement;
+                                target.setCustomValidity('Por favor, insira um email válido.');
+                            } }
+                            onInput={ event => {
+                                const target = event.target as HTMLInputElement;
+                                target.setCustomValidity('');
+                            } }
+                            value={ registerValues.email }
+                            onChange={ handleChange }
+                            className='text-center'
+                        />
+                    </label>
+
+                    <label className='flex gap-4' htmlFor="password">
+                        <h2>senha</h2>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="insira sua senha"
+                            minLength={ 6 }
+                            maxLength={ 16 }
+                            onInvalid={ event => {
+                                const target = event.target as HTMLInputElement;
+                                target.setCustomValidity('Por favor, insira uma senha válida.');
+                            } }
+                            onInput={ event => {
+                                const target = event.target as HTMLInputElement;
+                                target.setCustomValidity('');
+                            } }
+                            value={ registerValues.password }
+                            onChange={ handleChange }
+                            className='text-center'
+                        />
+                    </label>
+                    { loginErrorMessage && (
+                        <p
+                            className="text-red-600"
+                        >
+            Usuário ou senha inválidos
+                        </p>
+                    ) }
+                    <button
+                        type="submit"
+                        className={ `${ isDisable() ? 'bg-gray-300 ' : 'bg-green-700 ' }p-3 text-white rounded` }
+                        disabled={ isDisable() }
+                    >
+                        Login
+                    </button>
                 </form>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 }
+
