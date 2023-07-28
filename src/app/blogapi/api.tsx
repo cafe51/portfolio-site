aa6d114f94656f9b348c7e2a70dd4759ad6e996e
cafe51 <cafecafe51@hotmail.com>
2023-07-28 17:15:29 -0400

    fix: api

--- src/app/blogapi/api.tsx
+++ src/app/blogapi/api.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { CategoryType, NewPostType, NewUserType } from './types';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001/',
-    // baseURL: 'https://blog-api-japhe.up.railway.app/',
+    // baseURL: 'http://localhost:3001/',
+    baseURL: 'https://blog-api-japhe.up.railway.app/',
 });
 
 export const loginRequestApi = async(body: {email: string, password: string}) => {
